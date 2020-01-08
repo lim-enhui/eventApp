a17656352f40b5d37d1204265345daf2ee7dbb29
Lim En Hui <elim@Lims-MacBook-Pro.local>
2020-01-09 06:48:44 +0800

    Update at firebase functions 1.09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,15 @@ import { Store } from "@ngrx/store";
 import * as fromAppActions from "./store/app.actions";
 import * as fromAppReducer from "./store/app.reducer";
 
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed
+} from "@capacitor/core";
+
+const { PushNotifications } = Plugins;
+
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html"
@@ -37,6 +46,39 @@ export class AppComponent implements OnInit {
     console.log("App Initialization");
     this.store.dispatch(fromAppActions.loadGeoLocation());
     this.store.dispatch(fromAppActions.loadEvents());
+
+    // Register with Apple / Google to receive push via APNS/FCM
+    PushNotifications.register();
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener(
+      "registration",
+      (token: PushNotificationToken) => {
+        alert("Push registration success, token: " + token.value);
+        console.log(token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener("registrationError", (error: any) => {
+      alert("Error on registration: " + JSON.stringify(error));
+    });
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener(
+      "pushNotificationReceived",
+      (notification: PushNotification) => {
+        alert("Push received: " + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener(
+      "pushNotificationActionPerformed",
+      (notification: PushNotificationActionPerformed) => {
+        alert("Push action performed: " + JSON.stringify(notification));
+      }
+    );
   }
 
   closeMenu() {
